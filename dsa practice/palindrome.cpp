f6822364e7726ff586472b09fb47db80463e29a2
swarna kumari <118806429+Swarna2102@users.noreply.github.com>
2025-06-26 11:10:24 +0530

    Update palindrome.cpp

--- dsa practice/palindrome.cpp
+++ dsa practice/palindrome.cpp
@@ -4,9 +4,9 @@ public:
         int left=0;
         int right=s.size()-1;
         while(left<right){
-            while(left<right && !isalnum(s[left]))left++;
+            while(left<right && !isalnum(s[left]))left++; //isalnum is used to check for alpha numeric ,if space or any other thing is found it will move forward ignoring it 
             while(left<right && !isalnum(s[right]))right--;
-            if(tolower(s[left])!=tolower(s[right])){
+            if(tolower(s[left])!=tolower(s[right])){  //tolower use isliye kiya hai kyuki capital letter ko bhi hum small krde
                 return false;
             }
             left++;
@@ -16,3 +16,4 @@ public:
         
     }
 };
+
